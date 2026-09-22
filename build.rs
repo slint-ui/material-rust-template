aa0c413896f69266f89ace7f39aa09b1745c4053
Ioan Michnea <ioan.michnea@slint.dev>
2026-09-22 17:39:07 +0300

    Update to Material 1.1.0 and Slint 1.18

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
         std::collections::HashMap::from([(
             "material".to_string(),
             std::path::Path::new(&std::env::var_os("CARGO_MANIFEST_DIR").unwrap())
-                .join("material-1.0/material.slint"),
+                .join("material-1.1.0/material.slint"),
         )]),
     );
     slint_build::compile_with_config("ui/main.slint", config).unwrap();
